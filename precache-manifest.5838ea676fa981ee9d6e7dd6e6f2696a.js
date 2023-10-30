54fba9990a8e6e891f9d11582e157cd1d401f034
Andrei Kislichenko <android.2net@gmail.com>
2023-10-30 13:33:05 -0400

    Updates

---
+++ precache-manifest.5838ea676fa981ee9d6e7dd6e6f2696a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "62278dfecb094a4b1879",
+    "url": "/logicgame/static/css/main.6b959344.chunk.css"
+  },
+  {
+    "revision": "62278dfecb094a4b1879",
+    "url": "/logicgame/static/js/main.62278dfe.chunk.js"
+  },
+  {
+    "revision": "f1b021f7a58c6879e42f",
+    "url": "/logicgame/static/js/runtime~main.f1b021f7.js"
+  },
+  {
+    "revision": "94454794203b421e7044",
+    "url": "/logicgame/static/js/2.94454794.chunk.js"
+  },
+  {
+    "revision": "6930929ac8820f37c42c",
+    "url": "/logicgame/static/js/3.6930929a.chunk.js"
+  },
+  {
+    "revision": "4928e5a2f002bafc6583",
+    "url": "/logicgame/static/css/4.c3914b1a.chunk.css"
+  },
+  {
+    "revision": "4928e5a2f002bafc6583",
+    "url": "/logicgame/static/js/4.4928e5a2.chunk.js"
+  },
+  {
+    "revision": "65dd4911b34ec1c7aab90f883e1dc438",
+    "url": "/logicgame/index.html"
+  }
+];
