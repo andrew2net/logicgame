a7ff5f53759727d68f9491f6460588b04011c91e
Andrei Kislichenko <android.2net@gmail.com>
2025-12-29 22:52:58 -0500

    Updates

---
+++ precache-manifest.69772077ad555e67b49acfaad92e0475.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3b89bdb807e9341e1a51cd11d6cb98",
+    "url": "/logicgame/index.html"
+  },
+  {
+    "revision": "57e016bdc22b54f9d811",
+    "url": "/logicgame/static/css/4.f110db31.chunk.css"
+  },
+  {
+    "revision": "9652ea7b3d79efb31b8e",
+    "url": "/logicgame/static/css/main.937deb4a.chunk.css"
+  },
+  {
+    "revision": "a7a9938e7363dea6f6d8",
+    "url": "/logicgame/static/js/2.b76b1060.chunk.js"
+  },
+  {
+    "revision": "27a8cca68a2dc47219653d8c544ad8ad",
+    "url": "/logicgame/static/js/2.b76b1060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb1ae462b63f47aa2a9",
+    "url": "/logicgame/static/js/3.6aec925e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/logicgame/static/js/3.6aec925e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e016bdc22b54f9d811",
+    "url": "/logicgame/static/js/4.a332a00a.chunk.js"
+  },
+  {
+    "revision": "9652ea7b3d79efb31b8e",
+    "url": "/logicgame/static/js/main.4fb0dfb0.chunk.js"
+  },
+  {
+    "revision": "3af2e00ade587aa155fd",
+    "url": "/logicgame/static/js/runtime-main.41326200.js"
+  }
+]);
